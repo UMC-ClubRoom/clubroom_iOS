32bd1aa541247383d2be2a309cad2999e39202de
banxzxx <jwbang1643@gmail.com>
2023-01-16 18:10:16 +0900

    intinal commit

---
+++ get_group/get_group/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  get_group
+//
+//  Created by 방지원 on 2023/01/16.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
